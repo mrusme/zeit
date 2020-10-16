55a90954abd8305d69a6a134c2801627e8e19a00
マリウス <marius@twostairs.co>
2020-10-16 01:21:03 +0100

    Implemented erase command

---
+++ z/erase.go
@@ -0,0 +1,39 @@
+package z
+
+import (
+  "os"
+  "fmt"
+  "github.com/spf13/cobra"
+  "github.com/gookit/color"
+)
+
+var eraseCmd = &cobra.Command{
+  Use:   "erase ([flags]) [id]",
+  Short: "Erase activity",
+  Long: "Erase tracked activity.",
+  Args: cobra.ExactArgs(1),
+  Run: func(cmd *cobra.Command, args []string) {
+    user := GetCurrentUser()
+    id := args[0]
+
+    err := database.EraseEntry(user, id)
+    if err != nil {
+      fmt.Printf("%s %+v\n", CharError, err)
+      os.Exit(1)
+    }
+
+    fmt.Printf("%s erased %s\n", CharInfo, color.FgLightWhite.Render(id))
+    return
+  },
+}
+
+func init() {
+  rootCmd.AddCommand(eraseCmd)
+
+  var err error
+  database, err = InitDatabase()
+  if err != nil {
+    fmt.Printf("%s %+v\n", CharError, err)
+    os.Exit(1)
+  }
+}
