9a05dc1034e6020dc6d6cd33324b1c0ecca9ac89
マリウス <marius@xn--gckvb8fzb.com>
2023-01-14 21:55:41 -0500

    Added dateparse parsing, updated deps

--- go.mod
+++ go.mod
@@ -13,15 +13,16 @@ require (
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/tidwall/btree v1.5.2 // indirect
-	github.com/tidwall/gjson v1.14.3 // indirect
+	github.com/tidwall/btree v1.6.0 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/grect v0.1.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/rtred v0.1.2 // indirect
 	github.com/tidwall/tinyqueue v0.1.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 )
--- z/entryCmd.go
+++ z/entryCmd.go
@@ -1,82 +1,83 @@
 package z
 
 import (
-  "os"
-  "fmt"
-  "time"
-  "strings"
-  "github.com/spf13/cobra"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/araddon/dateparse"
+	"github.com/spf13/cobra"
 )
 
 var entryCmd = &cobra.Command{
-  Use:   "entry ([flags]) [id]",
-  Short: "Display or update activity",
-  Long: "Display or update tracked activity.",
-  Args: cobra.ExactArgs(1),
-  Run: func(cmd *cobra.Command, args []string) {
-    user := GetCurrentUser()
-    id := args[0]
+	Use:   "entry ([flags]) [id]",
+	Short: "Display or update activity",
+	Long:  "Display or update tracked activity.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		user := GetCurrentUser()
+		id := args[0]
 
-    entry, err := database.GetEntry(user, id)
-    if err != nil {
-      fmt.Printf("%s %+v\n", CharError, err)
-      os.Exit(1)
-    }
+		entry, err := database.GetEntry(user, id)
+		if err != nil {
+			fmt.Printf("%s %+v\n", CharError, err)
+			os.Exit(1)
+		}
 
-    if begin != "" || finish != "" || project != "" || notes != "" || task != "" {
-      if begin != "" {
-        entry.Begin, err = time.Parse(time.RFC3339, begin)
-        if err != nil {
-          fmt.Printf("%s %+v\n", CharError, err)
-          os.Exit(1)
-        }
-      }
+		if begin != "" || finish != "" || project != "" || notes != "" || task != "" {
+			if begin != "" {
+				entry.Begin, err = dateparse.ParseAny(begin)
+				if err != nil {
+					fmt.Printf("%s %+v\n", CharError, err)
+					os.Exit(1)
+				}
+			}
 
-      if finish != "" {
-        entry.Finish, err = time.Parse(time.RFC3339, finish)
-        if err != nil {
-          fmt.Printf("%s %+v\n", CharError, err)
-          os.Exit(1)
-        }
-      }
+			if finish != "" {
+				entry.Finish, err = dateparse.ParseAny(finish)
+				if err != nil {
+					fmt.Printf("%s %+v\n", CharError, err)
+					os.Exit(1)
+				}
+			}
 
-      if project != "" {
-        entry.Project = project
-      }
+			if project != "" {
+				entry.Project = project
+			}
 
-      if task != "" {
-        entry.Task = task
-      }
+			if task != "" {
+				entry.Task = task
+			}
 
-      if notes != "" {
-        entry.Notes = strings.Replace(notes, "\\n", "\n", -1)
-      }
+			if notes != "" {
+				entry.Notes = strings.Replace(notes, "\\n", "\n", -1)
+			}
 
-      _, err = database.UpdateEntry(user, entry)
-      if err != nil {
-        fmt.Printf("%s %+v\n", CharError, err)
-        os.Exit(1)
-      }
-    }
+			_, err = database.UpdateEntry(user, entry)
+			if err != nil {
+				fmt.Printf("%s %+v\n", CharError, err)
+				os.Exit(1)
+			}
+		}
 
-    fmt.Printf("%s %s\n", CharInfo, entry.GetOutput(true))
-    return
-  },
+		fmt.Printf("%s %s\n", CharInfo, entry.GetOutput(true))
+		return
+	},
 }
 
 func init() {
-  rootCmd.AddCommand(entryCmd)
-  entryCmd.Flags().StringVarP(&begin, "begin", "b", "", "Update date/time the activity began at\n\nUse RFC3339 format.")
-  entryCmd.Flags().StringVarP(&finish, "finish", "s", "", "Update date/time the activity finished at\n\nUse RFC3339 format.")
-  entryCmd.Flags().StringVarP(&project, "project", "p", "", "Update activity project")
-  entryCmd.Flags().StringVarP(&notes, "notes", "n", "", "Update activity notes")
-  entryCmd.Flags().StringVarP(&task, "task", "t", "", "Update activity task")
-  entryCmd.Flags().BoolVar(&fractional, "decimal", false, "Show fractional hours in decimal format instead of minutes")
+	rootCmd.AddCommand(entryCmd)
+	entryCmd.Flags().StringVarP(&begin, "begin", "b", "", "Update date/time the activity began at")
+	entryCmd.Flags().StringVarP(&finish, "finish", "s", "", "Update date/time the activity finished at")
+	entryCmd.Flags().StringVarP(&project, "project", "p", "", "Update activity project")
+	entryCmd.Flags().StringVarP(&notes, "notes", "n", "", "Update activity notes")
+	entryCmd.Flags().StringVarP(&task, "task", "t", "", "Update activity task")
+	entryCmd.Flags().BoolVar(&fractional, "decimal", false, "Show fractional hours in decimal format instead of minutes")
 
-  var err error
-  database, err = InitDatabase()
-  if err != nil {
-    fmt.Printf("%s %+v\n", CharError, err)
-    os.Exit(1)
-  }
+	var err error
+	database, err = InitDatabase()
+	if err != nil {
+		fmt.Printf("%s %+v\n", CharError, err)
+		os.Exit(1)
+	}
 }
